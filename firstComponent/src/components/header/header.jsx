ee1b5b4027dfb0d0ea77e49b1fbbdb819f95e6e2
eduardowada <156962925+eduardowada@users.noreply.github.com>
2024-09-03 17:00:45 -0300

    Header finalizado

---
+++ firstComponent/src/components/header/header.jsx
@@ -0,0 +1,18 @@
+import './header.css'
+
+function Header() {
+    return (
+        <header className='navbar-custom'>
+            <div className='navbar-title'>ExpeReact</div>
+            <nav>
+                <ul className='navbar-items'>
+                    <li>Home</li>
+                    <li>Contato</li>
+                    <li>Sobre nós</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
